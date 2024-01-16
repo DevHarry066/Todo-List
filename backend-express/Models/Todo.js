e765d0dd055e322cc23da7bcd42fe31a2c8f721a
Hariom <82692072+DevHarry066@users.noreply.github.com>
2024-01-16 09:21:39 -0800

    Added User and Todo Model

---
+++ backend-express/Models/Todo.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const slugify = require('slugify');
+
+const TodoSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: [true, 'Please add a title'],
+        unique: true,
+        trim: true,
+        maxlength: [50, 'Title can not be more than 50 characters']
+    },
+    slug: String,
+    description: {
+        type: String,
+        required: [true, 'Please add a description'],
+        maxlength: [500, 'Description can not be more than 500 characters']
+    },
+    isDone: {
+        type: Boolean,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    },
+    user: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+        required: [true]
+    },
+},
+    {
+        toJSON: { virtuals: true },
+        toObject: { virtuals: true }
+    });
+
+module.exports = mongoose.model('Todo', TodoSchema);
---
+++ backend-express/Models/User.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please enter name']
+    },
+    email: {
+        type: String,
+        match: [
+            /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+            'Please add a valid email'
+        ],
+        required: [true, 'Please enter email'],
+        unique: true
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user'
+    },
+    password: {
+        type: String,
+        required: [true, 'Please enter password'],
+        minLength: 6,
+        select: false
+    },
+    resetPasswordToken: String,
+    resetPasswordExpire: Date,
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+});
+
+UserSchema.pre('save', async function () {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+UserSchema.methods.getSignedJwtToken = function () {
+    return jwt.sign({
+        _id: this._id,
+    }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRE // expires in 24 hours
+    });
+};
+
+UserSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+module.exports = mongoose.model('User', UserSchema);
